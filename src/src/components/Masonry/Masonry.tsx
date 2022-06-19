16abec22aa5f474d880322dea0becf831cca5c3b
wswmsword <cjzjzzz@gmail.com>
2022-06-19 23:15:54 +0800

    Add rollup plugins: postcss, terser, peer-deps-external

--- src/src/components/Masonry/Masonry.tsx
+++ src/src/components/Masonry/Masonry.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import styles from "./Masonry.module.css";
 
 export interface MasonryProps {
 	label: string;
 }
 
 const Masonry = (props: MasonryProps) => {
-	return <div>{props.label}</div>;
+	return <div className={styles.purple}>{props.label}</div>;
 };
 
 export default Masonry;
